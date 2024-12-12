bdc79d74eceed91ea025d56732c8039f65cdb59c
qureshi2714 <msmuskanshaik65@gmail.com>
2024-12-12 15:16:39 +0530

    Add files via upload

---
+++ Collectionofarray.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Collectionofarray {
+    public static void main(String  args[]){
+        ArrayList<Integer> a= new ArrayList<>();
+        a.add(45);
+        a.add(18);
+        a.add(18);
+        a.add(12);
+        Collections.sort(a);
+        System.out.println(a);
+    }
+    
+    
+}
