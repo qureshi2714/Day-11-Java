fdb5075582e6e7fcf991a29d47cd45c595d2e453
qureshi2714 <msmuskanshaik65@gmail.com>
2024-12-12 12:26:34 +0530

    Add files via upload

---
+++ Arraycollections.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Arraycollections {
+    public static void main(String args[]){
+        ArrayList <Integer> a=new ArrayList<>();
+        a.add(45);
+        a.add(1);
+        a.add(18);
+        a.add(12);
+        Collections.sort(a);
+    System.out.println(a);
+    }
+
+
+    
+}
