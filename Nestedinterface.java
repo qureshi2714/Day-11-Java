2f174da5cf21ac22b67932364992b29398f97cc1
qureshi2714 <msmuskanshaik65@gmail.com>
2024-12-12 12:15:29 +0530

    Add files via upload

--- Nestedinterface.java
+++ Nestedinterface.java
@@ -1,1 +1,18 @@
+import java.util.*;
+interface out{
+    void outm();
+    interface in{
+        void innm();
+    }
+}
+class Nestedinterface implements out.in {
+    public void innm()
+    {
+        System.out.println("hello i'm inner method");
+    }
+    public static void main(String args[]){
+        Nestedinterface h= new Nestedinterface();
+        h.innm();
+    }
+}
 
